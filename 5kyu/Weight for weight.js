f47275b9f675eca213965f733300dc0c69659b1b
JIB2017 <blacuttjuan07@gmail.com>
2024-02-11 07:05:45 -0300

    Refined & completed kata

--- 5kyu/Weight for weight.js
+++ 5kyu/Weight for weight.js
@@ -1,24 +1,55 @@
 function orderWeight(strng) {
-  let list = strng.split(" ");
-  let array = [];
-  let result = [];
+  if (strng.length === 0) {
+    return "";
+  }
+  let list = strng.trim().split(" ");
+  let obj = [];
+  let aux;
+  let repeatedObj;
+  let repeatedNums = [];
+  let ordered = [];
 
   for (let i = 0; i < list.length; i++) {
-    array.push({
+    obj.push({
       weight: list[i],
-      sum: list[i].split("").reduce((acc, curr) => Number(acc) + Number(curr)),
+      sum:
+        list[i].length === 1
+          ? Number(list[i])
+          : list[i].split("").reduce((acc, curr) => Number(acc) + Number(curr)),
     });
   }
 
-  function ord(a, b) {
+  function order(a, b) {
     return a.sum - b.sum;
   }
 
-  array.sort(ord);
-
-  for (let weight of array) {
-    result.push(weight.weight);
+  function compare(numeros) {
+    numeros.sort((a, b) => {
+      return a.localeCompare(b);
+    });
+    return numeros;
   }
 
-  return result.join(" ");
+  obj.sort(order);
+
+  for (let i = 0; i < obj.length; i++) {
+    if (i + 1 < obj.length) {
+      if (obj[i].sum === obj[i + 1].sum && obj[i].sum !== aux) {
+        aux = obj[i].sum;
+        repeatedObj = obj.filter((e) => e.sum === aux);
+        for (let rep of repeatedObj) {
+          repeatedNums.push(rep.weight);
+        }
+        ordered.push(compare(repeatedNums));
+        repeatedNums = [];
+      }
+      if (obj[i].sum !== aux) {
+        ordered.push(obj[i].weight);
+      }
+    } else if (obj[i].sum !== aux) {
+      ordered.push(obj[i].weight);
+    }
+  }
+
+  return ordered.flat().toString().replaceAll(",", " ");
 }
